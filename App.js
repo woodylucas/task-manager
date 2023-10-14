70146661b8155060089872c0156630794214d1bc
woodylucas <wlucas0311@gmail.com>
2023-10-14 19:43:11 -0400

    initial

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useCallback } from "react";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import { Button } from "react-native";
+
+import GoalItem from "./src/components/GoalItem";
+import GoalInput from "./src/components//GoalInput";
+
+import * as Crypto from "expo-crypto";
+
+import {
+  StyleSheet,
+  View,
+  SafeAreaView,
+  StatusBar,
+  FlatList,
+} from "react-native";
 
 export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handleAddGoal = useCallback((text) => {
+    setCourseGoals((prevGoal) => [
+      { id: Crypto.randomUUID(), text },
+      ...prevGoal,
+    ]);
+
+    setModalVisible(false);
+  }, []);
+
+  const handleDeleteGoal = useCallback((id) => {
+    setCourseGoals((currentGoals) => {
+      return currentGoals.filter((goal) => goal.id !== id);
+    });
+  }, []);
+
+  const handleOpenModal = () => {
+    setModalVisible(true);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <Button
+          title="Add New Goal"
+          color="#a065ec"
+          onPress={handleOpenModal}
+        />
+        <GoalInput
+          onAddGoal={handleAddGoal}
+          modalVisible={modalVisible}
+          onClose={setModalVisible}
+        />
+        <View style={styles.goalsContainer}>
+          <FlatList
+            data={courseGoals}
+            renderItem={({ item }) => (
+              <GoalItem item={item} onDelete={handleDeleteGoal} />
+            )}
+            key={({ item: { id } }) => id}
+          />
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style="light" />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: StatusBar.currentHeight,
+    marginLeft: 8,
+    marginRight: 24,
+  },
+
+  goalsContainer: {
+    flex: 5,
   },
 });
